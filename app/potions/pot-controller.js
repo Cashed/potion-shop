9e658c049a128ff87b82b7ab829b568d7feda86f
Veronica Moreno <frau.veronika@gmail.com>
2016-05-30 23:25:59 -0700

    arrows for changing reviews

--- app/potions/pot-controller.js
+++ app/potions/pot-controller.js
@@ -16,15 +16,6 @@
     vm.bagSize = 0;
     vm.review = 0;
 
-    var getReview = $interval(
-      function(){
-        if (vm.review === 4) {
-          vm.review = 0;
-        }
-        else {
-          vm.review++;
-        }
-      }, 7000);
 
     vm.addToCart = function(pot, qty) {
       if (qty > 0) {
@@ -43,5 +34,27 @@
 
       vm.pullDown = category;
     };
+
+    vm.changeReview = function(direction) {
+      if (direction === 1 && vm.review === 4) {
+        vm.review = 0;
+      }
+      else if (direction === -1 && vm.review === 0) {
+        vm.review = 4;
+      }
+      else {
+        vm.review += direction;
+      }
+    };
+
+    var getReview = $interval(
+      function(){
+        if (vm.review === 4) {
+          vm.review = 0;
+        }
+        else {
+          vm.review++;
+        }
+      }, 7000);
   }
 })();
